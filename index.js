4b19ccb76f5bd763e501bf71f48686ea68620b62
dSecret <ajrathore786@gmail.com>
2018-02-11 15:08:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+const axios = require('axios')
+const token="483816962:AAELmfMG9kOWRhN83XRtHI9LeFdkadlo6aE"
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
+
+app.post('/new-message', function(req, res) {
+  const {message} = req.body
+  axios.post('https://api.telegram.org/bot'+token+'/sendMessage', {
+    chat_id: message.chat.id,
+    text: 'Rathore'
+  })
+    .then(response => {
+      res.end('ok')
+    })
+    .catch(err => {
+      res.end('Error :' + err)
+    })
+
+});
+app.get('/',(req,res)=>{
+  res.send("its working")
+})
+// Finally, start our server
+app.listen(process.env.PORT||80, function() {
+  console.log('Telegram app listening on port 3000!');
+});
